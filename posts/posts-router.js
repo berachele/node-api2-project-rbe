66fab26611d5f791c470141b12648d73948a4433
Rachele Edwards <dot.com1472@gmail.com>
2020-05-05 15:58:33 -0600

    successfully compiling and requests get /:id/comments

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -9,7 +9,7 @@ router.post('/', (req, res) => {})
 //POST    /api/posts/:id/comments  Creates a comment for the post with the specified id using information sent inside of the `request body`.                                                                   
 router.post('/:id/comments', (req, res) => {})
 
-//GET ✅     /api/posts               Returns an array of all the post objects contained in the database.                                                                                                         
+//GET 🥳     /api/posts               Returns an array of all the post objects contained in the database.                                                                                                         
 router.get('/', (req, res) => {
     Blog.find()
     .then(success => {
@@ -23,7 +23,7 @@ router.get('/', (req, res) => {
     })
 })
 
-//GET     /api/posts/:id           Returns the post object with the specified id.                                                                                                                              
+//GET 🥳    /api/posts/:id           Returns the post object with the specified id.                                                                                                                              
 router.get('/:id', (req, res) => {
     const id = req.params.id
     Blog.findById(id)
@@ -44,8 +44,26 @@ router.get('/:id', (req, res) => {
     })
 })
 
-//GET     /api/posts/:id/comments  Returns an array of all the comment objects associated with the post with the specified id.                                                                                 
-router.get('/:id/comments', (req, res) => {})
+//GET 🥳    /api/posts/:id/comments  Returns an array of all the comment objects associated with the post with the specified id.                                                                                 
+router.get('/:id/comments', (req, res) => {
+    const id = req.params.id
+    Blog.findPostComments(id)
+    .then(success => {
+        if(success){
+            res.status(200).json(success)
+        }else {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist."
+            })
+        }
+    })
+    .catch(err => {
+        console.log({err})
+        res.status(500).json({
+            error: "The comments information could not be retrieved."
+        })
+    })
+})
 
 //DELETE  /api/posts/:id           Removes the post with the specified id and returns the **deleted post object**. 
 //You may need to make additional calls to the database in order to satisfy this requirement. 
